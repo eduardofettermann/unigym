84ef0aa26c4b3e46807dbfb7f722ef07a5c61bff
Eduardo Fettermann <eduardofettermann212@gmail.com>
2026-05-18 19:45:07 -0300

    adiciona base do projeto spring

---
+++ src/main/java/com/unigym/UnigymApplication.java
@@ -0,0 +1,13 @@
+package com.unigym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnigymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnigymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unigym/UnigymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unigym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnigymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
